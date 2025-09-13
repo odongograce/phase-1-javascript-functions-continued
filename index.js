e2f074a35d72e80eca9f97d903fc67ad09995cd7
Grace Odongo <graceakinyi024@gmail.com>
2025-09-13 10:20:20 +0300

    updatesindex.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+const mondayWork = function (action = "go to the office") {
+    return `This Monday, I will ${action}.`
+}
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "a hard worker") {
+    return `You are ${wrapper}${adjective}${wrapper}!`
+  }
+}
+
+
+
+
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+console.log(wrapAdjective());
+console.log(wrapAdjective(wrapper = "||", adjective ="a dedicated programmer"));
